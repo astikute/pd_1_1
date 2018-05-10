11db93e919f93a9217e3d76cec115e53ef37d6f7
astikute <agnese.stikute@gmail.com>
2018-05-10 18:51:18 +0300

    patstavigais darbs
    pd_1_1

---
+++ pd_1_1/pd_1_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pd_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ievadi savu vardu: ");
+            string vards = Console.ReadLine();
+
+
+
+            Console.Read();
+
+        }
+    }
+}
