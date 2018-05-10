9c9565162832b1f0a5e502df8595e9347b0d15f1
astikute <agnese.stikute@gmail.com>
2018-05-10 19:19:27 +0300

    patstavigais darbs
    funkcijas, if-else

--- pd_1_1/pd_1_1/Program.cs
+++ pd_1_1/pd_1_1/Program.cs
@@ -10,10 +10,60 @@ namespace pd_1_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ievadi savu vardu: ");
+            //Uzdevums_1
+            Console.Write("Ievadi savu vardu: ");
             string vards = Console.ReadLine();
+            Console.Write("Ievadi savu uzvardu: ");
+            string uzvards = Console.ReadLine();
+            Console.WriteLine("Tevi sauc " + vards + " " + uzvards + "!");
 
+            //Uzdevums_2
+            Console.Write("Ievadi gadu: ");
+            int gads = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ievadi savu dzimsanas gadu: ");
+            int dz_gads = Convert.ToInt32(Console.ReadLine());
+            int vecums = gads - dz_gads;
+            Console.WriteLine("Tev ir " + vecums + ".");
 
+            //Uzdevums_3
+            Console.Write("Ievadiet rinka linijas radiusu: ");
+            double rlr = Convert.ToDouble(Console.ReadLine());
+            double rlr_D = 2 * rlr;
+            double rlr_S = 2 * rlr * 3.14;
+            Console.WriteLine("Rinka linijas diametrs ir {0} un laukums ir {1}", rlr_D, rlr_S + ".");
+
+            //Uzdevums_4
+            Console.Write("Ievadiet skaitli: ");
+            double skaitlis = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Mans skaitlis ir " + skaitlis + ".");
+
+            //Uzdevums_5
+            Console.Write("Ievadiet simbolu virkni: ");
+            string virkne = Console.ReadLine();
+            if (virkne == "macos programmet")
+            {
+                Console.WriteLine("Simbolu virkne ir vienada!");
+            }
+            else
+            {
+                Console.WriteLine("Simbolu virkne NAV vienada!");
+            }
+
+            //Uzdevums_6
+            Console.WriteLine("Vai gribat sasveicinaties?");
+            string izvele = Console.ReadLine();
+            if (izvele == "ja")
+            {
+                Console.Write("Ievadi savu vardu: ");
+                vards = Console.ReadLine();
+                Console.Write("Ievadi savu uzvardu: ");
+                uzvards = Console.ReadLine();
+                Console.WriteLine("Tevi sauc " + vards + " " + uzvards + "!");
+            }
+            else
+            {
+                Console.WriteLine("Zel gan!");
+            }
 
             Console.Read();
 
